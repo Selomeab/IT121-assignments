f4e242f7a86c856dcd7d235f2829eead7015beff
Selomeab <selomea.beyene@gmail.com>
2025-01-21 00:17:09 -0800

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,11 @@
+// Declaring variables
+var userName = "Selomea"; 
+let age = 23;       
+const city = "Seattle"; 
+favoriteColor = "Black"; 
+
+// Printing variables to the console
+console.log(userName);        
+console.log(age);         
+console.log(city);        
+console.log(favoriteColor); 
