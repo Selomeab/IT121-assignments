aadeef5a7b57f30ff169ebe503c2ca395cc6cd8e
Selomeab <selomea.beyene@gmail.com>
2025-01-21 00:25:36 -0800

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,8 @@
+//Assign a value to a variable using let
+let myAge = 12;
+
+//Reassigning a different value to the variable
+myAge = 23;
+
+//Printing the value of the variable to the console
+console.log(myAge);
