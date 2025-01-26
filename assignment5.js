9ce257f188757543ac2e8250d4c05c03ba0f4ab1
Selomeab <selomea.beyene@gmail.com>
2025-01-26 15:12:35 -0800

    Add files via upload

---
+++ assignment5.js
@@ -0,0 +1,16 @@
+//Selomea Beyene
+
+let equationAddition = 11 + 23; 
+console.log(equationAddition); // Output: 34
+
+let equationSubtraction = 24 - 4; 
+console.log(equationSubtraction); // Output: 20
+
+let equationMultiplication = 12 * 12; 
+console.log(equationMultiplication); // Output: 144
+
+let equationDivision = 20 / 4; 
+console.log(equationDivision); // Output: 5
+
+let equationModulus = 11 % 3; 
+console.log(equationModulus); // Output: 2
