70a45e2a6bbb47c93faf3de186f60b411c6fdbfb
Selomeab <selomea.beyene@gmail.com>
2025-01-26 15:24:19 -0800

    Add files via upload

---
+++ assignment6.js
@@ -0,0 +1,9 @@
+//Variables 
+let firstName = "Selomea";
+let lastName = "Beyene";
+let city = "Seattle";
+let occupation = "Year Up student";
+let funfact = "traveling";
+
+console.log(firstName + " " + lastName + " lives in " + city + ". She is a " + occupation + " and enjoys " + funfact + ".");
+//Output: Selomea Beyene lives in Seattle. She is a Year Up student and enjoys traveling.
